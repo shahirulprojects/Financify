8a75ef9152f82fb91a6dcd74d9d84bd41e23885e
shahirulprojects <shahiruliimalo@gmail.com>
2024-05-20 00:08:31 +0800

    feat:add basic sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar />
       {children}
     </main>
   );
