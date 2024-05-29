8ff190649b7a7117b2a4032ac8ed78592b8f1a69
shahirulprojects <shahiruliimalo@gmail.com>
2024-05-29 19:08:25 +0800

    feat:continue server actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,10 @@
-"use server"; // because we want to do server action so we have to use "use server"
+"use server";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+// because we want to do server action so we have to use "use server"
 
 export const signIn = async () => {
   try {
@@ -7,10 +13,42 @@ export const signIn = async () => {
     console.error("Error", error);
   }
 };
+
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   try {
     // Create a user account
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+
+    // Create a session
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    // we do it like this because in NextJS we cannot passed a large object through server action, we have to stringify it first
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
